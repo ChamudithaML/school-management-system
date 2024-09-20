6894f42c3dff51e75f21734db579597e33ab22f3
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-09-20 19:50:30 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Grades from './pages/Grades';
+import Classes from './pages/Classes';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/grades' element={<Grades />} />
+          <Route path='/grade1' element={<Classes />} />
+        </Routes>
+        <Footer />
+      </div>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
